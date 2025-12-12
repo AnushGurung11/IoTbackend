113f68c6126b485d4dac3b60276523492f076494
AnushGurung11 <anushgrg15@gmai.com>
2025-12-12 14:51:49 +0545

    Added controller, post fuction and get all record function

---
+++ controller/DataController.js
@@ -0,0 +1,28 @@
+// Importing the model for controlling the data flow 
+import Data from '../model/Data.js'; 
+
+// Created a function for posting the new entry
+// async function is used which will not stop the flow of the program
+export const addData = async (req, res) => {
+
+    try{
+        const {waterLevel, servoStatus} = req.body;
+        const newData = new Data({waterLevel, servoStatus}); 
+        await newData.save(); 
+        res.status(201).json({message: 'Data saved successfully'}); 
+
+    }catch(error){
+        res.status(500).json({error: 'Failed to save data'});
+    }
+}
+
+
+// function for getting all the data from the data base
+export const getData = async(req, res)=>{
+    try{
+        const allData = await Data.find().sort({createdAt: -1 }); 
+        res.status(200).json(allData); 
+    }catch(error){
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+}
